bbf37079190847c629bf1af43aa67f0ff13b15cd
花霗HaLin <47058874+ABCDEFGHIJKLMOPQRSTUVWXY@users.noreply.github.com>
2023-07-03 10:21:30 +0800

    Update 1.py

--- 1/1.py
+++ 1/1.py
@@ -1,11 +1,24 @@
+import numpy as np
 import pandas as pd
 
-data = {id: [202001, 202002, 202003, 202004, 202005, 202006, 202007, 202008, 202009, 202010],
-Chinese: [98, 67, 84, 88, 78, 90, 93, 75, 82, 87],
-Math: [92, 80, 73, 76, 88, 78, 90, 82, 77, 69],
-English: [88, 79, 90, 73, 79, 83, 81, 91, 71, 78]
-}
+df=pd.DataFrame({
+'Id': [202001, 202002, 202003, 202004, 202005, 202006, 202007, 202008, 202009, 202010],
+'Chinese': [98, 67, 84, 88, 78, 90, 93, 75, 82, 87],
+'Math': [92, 80, 73, 76, 88, 78, 90, 82, 77, 69],
+'English': [88, 79, 90, 73, 79, 83, 81, 91, 71, 78]
+})
 
-df = pd.DataFrame(data)
+score=np.array(df)
+score=np.column_stack((score,np.mean(score[:,1:3],axis=1)))
+score=np.column_stack((score,np.sum(score[:,1:3],axis=1)))
+score=np.column_stack((score,np.max(score[:,1:3],axis=1)))
+score=np.column_stack((score,np.min(score[:,1:3],axis=1)))
+score=np.column_stack((score,np.ptp(score[:,1:3],axis=1)))
+score=np.column_stack((score,np.var(score[:,1:3],axis=1)))
 
-df
+result=pd.DataFrame(score)
+
+for i in range(0,len(df)):
+    for j in range(4,len(df.columns)):
+        result.iloc[i][j]=int(result.iloc[i][j])
+print(result)
