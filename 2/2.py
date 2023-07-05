7888d10b77686e871e57082fdf7f92d9c0decc3d
花霗HaLin <47058874+ABCDEFGHIJKLMOPQRSTUVWXY@users.noreply.github.com>
2023-07-05 08:54:05 +0800

    2.3

--- 2/2.py
+++ 2/2.py
@@ -5,14 +5,14 @@ df = pd.read_excel('/home/mw/input/pandas1206855/pandas120.xlsx')
 df.head()
 
 
-#
+#提取学历为本科，工资在25k-35k的数据
 salary_25k_35k_df = df[df['salary'] == '25k-30k']
 edu_df = salary_25k_35k_df[salary_25k_35k_df['education'] == '本科']
 df1 = edu_df
 
 
-
-end_40k_df = df[df['salary']].str.match('*40k')
+#提取salary列中以'40k'结尾的数据
+end_40k_df = df[df['salary'].str.endswitch('40k')]
 df2 = end_40k_df
 
 
