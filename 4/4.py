8e29542a143312caad09308c191529d85dcd3235
XiaoGu-G2020 <1823355358@qq.com>
2023-07-07 08:48:26 +0800

    添加了数据集

--- 4/4.py
+++ 4/4.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
-import seaborn as sns
 
 data = pd.read_excel('/home/mw/input/pandas1206855/600000.SH.xls')
 
